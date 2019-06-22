e4a1e7f258caaf7af81b9f9504865606b3140e75
mxwilbur <mxwilbur@outlook.com>
2019-06-22 23:04:57 +0000

    added event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,13 @@ class EventsController < ApplicationController
 
 
   def index
+    @events = Event.all
+    @photos = Photo.new
+
   end
   
+  def new
+    @events = Event.new
+    @photos = Photo.new
+  end
 end
